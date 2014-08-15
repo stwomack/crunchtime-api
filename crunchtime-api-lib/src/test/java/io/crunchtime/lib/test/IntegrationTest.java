7c881d7ee9050683740239b6240ab6554e3f9d2f
Steve Womack <stephen.womack@charter.com>
2014-08-15 13:47:10 -0500

    Fixing Spring dependencies

--- crunchtime-api-lib/src/test/java/io/crunchtime/lib/test/IntegrationTest.java
+++ crunchtime-api-lib/src/test/java/io/crunchtime/lib/test/IntegrationTest.java
@@ -5,7 +5,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration({ "classpath:spring/application-context.xml" })
+@ContextConfiguration({ "classpath:spring/application-context.xml", "classpath:spring/data-context.xml" })
 public abstract class IntegrationTest {
 
 }
