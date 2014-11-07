69795baac65257b9a28a64545ebb86a06ec8fadb
Steve Womack <stwomack@gmail.com>
2014-11-07 14:33:27 -0600

    Slimming down app

--- crunchtime-api-lib/src/main/java/io/crunchtime/lib/dao/RedisDao.java
+++
@@ -1,9 +0,0 @@
-package io.crunchtime.lib.dao;
-
-import io.crunchtime.lib.domain.CrunchtimeSerializable;
-
-public interface RedisDao<T extends CrunchtimeSerializable> {
-
-	public abstract void saveAndSendToBroker(String channel, T domainObject);
-
-}
--- crunchtime-api-rest/src/main/java/io/crunchtime/rest/resources/MeetingResourceImpl.java
+++ crunchtime-api-rest/src/main/java/io/crunchtime/rest/resources/MeetingResourceImpl.java
@@ -1,23 +1,19 @@
 package io.crunchtime.rest.resources;
 
 import io.crunchtime.lib.domain.Meeting;
-import io.crunchtime.lib.service.MeetingService;
+import io.crunchtime.lib.helpers.testdata.MeetingTestDataBuilder;
 
 import java.util.List;
 
 import javax.ws.rs.core.GenericEntity;
 import javax.ws.rs.core.Response;
 
-import org.springframework.beans.factory.annotation.Autowired;
-
 public class MeetingResourceImpl implements MeetingResource {
 
-	@Autowired
-	private MeetingService meetingService;
-
 	public Response getAllMeetings() {
-		List<Meeting> allMeetings = meetingService.findAll();
-		GenericEntity<List<Meeting>> entity = new GenericEntity<List<Meeting>>(allMeetings) {};
+		GenericEntity<List<Meeting>> entity = new GenericEntity<List<Meeting>>(
+				new MeetingTestDataBuilder().buildAll()) {
+		};
 		return Response.ok(entity).build();
 	}
 
